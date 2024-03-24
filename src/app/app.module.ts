0563f84ec7473fc0bb8b16da911c91807ec52827
MoYasser1 <mohamed0yasser@gmail.com>
2024-03-24 08:29:39 +0200

    start_project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { CartComponent } from './components/cart/cart.component';
+import { ProductsComponent } from './components/products/products.component';
+import { BrandsComponent } from './components/brands/brands.component';
+import { CategoriesComponent } from './components/categories/categories.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { NotfoundComponent } from './components/notfound/notfound.component';
+import { DetailsComponent } from './components/details/details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CartComponent,
+    ProductsComponent,
+    BrandsComponent,
+    CategoriesComponent,
+    FooterComponent,
+    LoginComponent,
+    RegisterComponent,
+    NotfoundComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule,
